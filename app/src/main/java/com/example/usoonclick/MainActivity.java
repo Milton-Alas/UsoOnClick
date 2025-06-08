788298108fd1f840239741271e7dc55d47fe3e54
david <mv12013@ues.edu.sv>
2025-06-08 02:28:13 -0600

    :sparkles: dialogos guia semana 14

--- app/src/main/java/com/example/usoonclick/MainActivity.java
+++ app/src/main/java/com/example/usoonclick/MainActivity.java
@@ -1,17 +1,30 @@
 package com.example.usoonclick;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,9 +32,14 @@ import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
     // Propiedades personalizadas de la clase MainActivity
     private static final int PICK_IMAGE_REQUEST = 1;
@@ -30,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
     ListView lstdepartamentos;
     ArrayList<String> items;
     ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,13 +56,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // Generando elemetos del componente ListView
         lstdepartamentos = findViewById(R.id.lstDepartamentos);
+
         items = new ArrayList<>();
-        items.add("1 - Ahuchapán");
+        items.add("1 - Ahuachapán");
         items.add("2 - Santa Ana");
         items.add("3 - Sonsonate");
         //Estableciendo el tipo de distribución de los elementos: lista simple
-                adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, items);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
         //Asignando la lista de elementos de la clase Adapter al
         //componente ListView
         lstdepartamentos.setAdapter(adapter);
@@ -59,81 +78,161 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v,
-                                                                            insets) -> {
-            Insets systemBars =
-                    insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top,
-                    systemBars.right, systemBars.bottom);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
         // Refencias a elementos de la UI que serán utilizados desde la capa lógica.
-        Switch swModo = findViewById(R.id.SwCambiarModo);
-        FloatingActionButton fab =
-                findViewById(R.id.floatingActionButton);
+        @SuppressLint("UseSwitchCompatOrMaterialCode") Switch swModo = findViewById(R.id.SwCambiarModo);
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
         EditText tvestadomodo = findViewById(R.id.edtEstadoModo);
-        CheckBox chkboxdesamodo =
-                findViewById(R.id.chboxDesactivacambiomodo);
+        CheckBox chkboxdesamodo = findViewById(R.id.chboxDesactivacambiomodo);
         RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        ImageButton imgbuttonfecha = findViewById(R.id.imgButtonFecha);
+        ImageButton imgbuttonhora = findViewById(R.id.imgButtonHora);
         // Manejo del evento onClick del elemento de tipo Switch
         swModo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Cambiando modo")
+                        .setMessage("Se cambiará el modo de la aplicación")
+                        .setPositiveButton("OK", null)
+                        .show();
                 boolean estado = ((Switch) view).isChecked();
-                if (estado){
-
+                if (estado) {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
                     tvestadomodo.setText("Modo oscuro esta activado");
-                }else{
-
+                } else {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                     tvestadomodo.setText("");
                 }
             }
         });
+
         // Manejo del evento onClick del elemento de tipo CheckBox
         chkboxdesamodo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (chkboxdesamodo.isChecked()){
-                    swModo.setEnabled(false);
-                }else{
-                    swModo.setEnabled(true);
-                }
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Confirmar cambio de modo")
+                        .setMessage("Esta seguro que desea deshabilitar el cambio de modo")
+                        .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                swModo.setEnabled(!chkboxdesamodo.isChecked());
+                            }
+                        })
+                        .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                chkboxdesamodo.setChecked(!chkboxdesamodo.isChecked());
+                            }
+                        })
+                        .show();
+            }
+        });
+        imgbuttonfecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int annio = LocalDate.now().getYear();
+                int mes = LocalDate.now().getMonthValue();
+                int dia = LocalDate.now().getDayOfMonth();
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @SuppressLint("SetTextI18n")
+                    @Override
+
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        String fecha = dayOfMonth + "/" + (month + 1) + "/" + year;
+                        tvestadomodo.setText("Fecha: " + fecha);
+                    }
+                }, annio, mes - 1, dia);
+                datePickerDialog.show();
+            }
+        });
+
+        imgbuttonhora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int hora = Calendar.getInstance().get(Calendar.HOUR_OF_DAY) + 1;
+                int minutos = Calendar.getInstance().get(Calendar.MINUTE) + 1;
+                TimePickerDialog timePickerDialog = new
+                        TimePickerDialog(MainActivity.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @SuppressLint("SetTextI18n")
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+                                @SuppressLint("DefaultLocale") String hora = hourOfDay + ":" + String.format("%02d", minute);
+                                tvestadomodo.setText("Hora : " + hora);
+                            }
+                        }, hora - 1, minutos, true);
+                timePickerDialog.show();
             }
         });
         // Manejo del evento onCheckedChanged que representa una acción equivalente a onClick
-        radioGroup.setOnCheckedChangeListener(new
-                                                      RadioGroup.OnCheckedChangeListener() {
-                                                          @Override
-                                                          public void onCheckedChanged(RadioGroup radioGroup, int
-                                                                  checkedId) {
-                                                              if (checkedId != 1) {
-                                                                  RadioButton radioButtonSelec =
-                                                                          findViewById(checkedId);
-                                                                  int indexbutton =
-                                                                          radioGroup.indexOfChild(radioButtonSelec);
-                                                                  if(indexbutton==1){
-                                                                      imgviewFoto.setVisibility(View.INVISIBLE);
-                                                                  }else{
-                                                                      imgviewFoto.setVisibility(View.VISIBLE);
-                                                                  }
-                                                              }
-                                                          }
-                                                      });
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                if (checkedId != 1) {
+                    RadioButton radioButtonSelec = findViewById(checkedId);
+                    int indexbutton = radioGroup.indexOfChild(radioButtonSelec);
+                    if (indexbutton == 1) {
+                        LayoutInflater inflater = getLayoutInflater();
+                        View dialogView =
+                                inflater.inflate(R.layout.dialogo_personalizado, null);
+                        EditText razonocultar =
+                                dialogView.findViewById(R.id.edtJustificacion);
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setView(dialogView);
+                        builder.setTitle("Venta - Razón para ocultar ");
+                        builder.setPositiveButton("Registrar", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String razon = razonocultar.getText().toString();
+
+                                tvestadomodo.setText(razon);
+
+                            }
+                        });
+                        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                tvestadomodo.setText("");
+                            }
+                        });
+                        builder.show();
+                        imgviewFoto.setVisibility(View.INVISIBLE);
+                    } else {
+                        imgviewFoto.setVisibility(View.VISIBLE);
+                        tvestadomodo.setText("");
+                    }
+                }
+            }
+        });
         // Manejo del evento onItemClick del elemento de tipo ListView
-        lstdepartamentos.setOnItemClickListener(new
-                                                        AdapterView.OnItemClickListener() {
-                                                            @Override
-                                                            public void onItemClick(AdapterView<?> adapterView, View
-                                                                    view, int position, long id) {
-                                                                String elementoseleccionado = items.get(position);
-                                                                View rooLayout;
-                                                                rooLayout = findViewById(R.id.main);
-                                                                Snackbar.make(rooLayout, "El elemento seleccionado es " +
-                                                                        elementoseleccionado, Snackbar.LENGTH_SHORT).show();
-                                                            }
-                                                        });
+        lstdepartamentos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                TextView itemselected = (TextView) view;
+                AlertDialog.Builder builder = new
+                        AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Ingrese el nuevo valor para el elemento seleccionado ");
+                final EditText nuevotexto = new EditText(MainActivity.this);
+                nuevotexto.setInputType(InputType.TYPE_CLASS_TEXT);
+                builder.setView(nuevotexto);
+                builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String textonuevo = nuevotexto.getText().toString();
+                        itemselected.setText(textonuevo);
+                    }
+                });
+                builder.setNegativeButton("Cancelar", null);
+                builder.show();
+            }
+        });
         // Manejo del evento onClick del elemento de tipo     FloatinActionButtom
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -145,26 +244,23 @@ public class MainActivity extends AppCompatActivity {
         chkboxdesamodo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (chkboxdesamodo.isChecked()){
+                if (chkboxdesamodo.isChecked()) {
                     swModo.setEnabled(false);
-                }else{
+                } else {
                     swModo.setEnabled(true);
                 }
             }
         });
         // Manejo del evento onItemClick del elemento de tipo ListView
-        lstdepartamentos.setOnItemClickListener(new
-                                                        AdapterView.OnItemClickListener() {
-                                                            @Override
-                                                            public void onItemClick(AdapterView<?> adapterView, View
-                                                                    view, int position, long id) {
-                                                                String elementoseleccionado = items.get(position);
-                                                                View rooLayout;
-                                                                rooLayout = findViewById(R.id.main);
-                                                                Snackbar.make(rooLayout, "El elemenot seleccionado es " +
-                                                                        elementoseleccionado, Snackbar.LENGTH_SHORT).show();
-                                                            }
-                                                        });
+        lstdepartamentos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                String elementoseleccionado = items.get(position);
+                View rooLayout;
+                rooLayout = findViewById(R.id.main);
+                Snackbar.make(rooLayout, "El elemenot seleccionado es " + elementoseleccionado, Snackbar.LENGTH_SHORT).show();
+            }
+        });
         // Manejo del evento onClick del elemento de tipo ImageView
         imgviewFoto.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -173,27 +269,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     // Metodo personalizado de la clase que se usa para crear un Intend
     // que servira para abrir la galeria de fotos y seleccionar una
     //imagen que sera cargada en el ImageView
-    private void OpenGalery(){
+    private void OpenGalery() {
         Intent i = new Intent(Intent.ACTION_PICK);
         i.setType("image/*");
-        startActivityForResult(i,PICK_IMAGE_REQUEST);
+        startActivityForResult(i, PICK_IMAGE_REQUEST);
     }
+
     // Método de la clse sobreescrito para optener la URI de la imagen
     //seleccionda en la galeria de fotos
     // y asignarla como valor a la propiedad URI del elemento ImageView
     @Override
-    protected void onActivityResult(int requestCode, int resultCode,
-                                    @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
-                && data != null && data.getData() != null) {
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
             imgURI = data.getData();
             imgviewFoto.setImageURI(imgURI);
         }
     }
+
     //Método de la clae para guardar cambios en el propiedad URI
     // Que servira para recuperar la imagen asiganda al elemento
     //ImageView cuando se haga un cambio de modo
